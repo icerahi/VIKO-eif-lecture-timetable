e50e5d3c910edce41c836f4f24de807fb57a2ac8
Md Imran Hasan <imranmdhasan07@gmail.com>
2025-02-26 23:47:18 +0200

    vite.config.js added for deploy

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/VIKO-eif-lecture-timetable/", // Replace <repository-name> with your actual repository name
+  // You can add other configurations here as needed
+});
