b8e7380b180187aa0c3e3429274c0f6bd7753a03
Md Imran Hasan <imranmdhasan07@gmail.com>
2025-04-02 10:11:58 +0300

    pwa added

--- viko_timetable/src/main.jsx
+++ viko_timetable/src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
--- viko_timetable/vite.config.js
+++ viko_timetable/vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "./",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "VIKO EIF Timetable",
+        short_name: "PWA App",
+        description: "A simple PWA using React and Vite",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ],
   build: {
     outDir: "../docs",
   },
