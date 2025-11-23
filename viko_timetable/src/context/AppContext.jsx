66f625f728242d2a7d20bff4c77e41472f2e749e
Md Imran Hasan <imranmdhasan07@gmail.com>
2025-11-23 10:36:40 -0500

    backend server changed

--- viko_timetable/src/context/AppContext.jsx
+++ viko_timetable/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ export const AppContext = createContext();
 
 const DataProvider = ({ children }) => {
   const [test, setTest] = useState(null);
-  const API_URL = "https://overseas-vyky-icerahi-d9f7baf3.koyeb.app";
+  const API_URL = "https://romantic-lorenza-zanjarwhite-5f028c50.koyeb.app";
   // const API_URL = "https://overseas-vyky-icerahi-d9f7baf3.koyeb.app";
   // const API_URL = "https://viko-schedule-app.fly.dev";
   // const API_URL = "http://localhost:3000";
